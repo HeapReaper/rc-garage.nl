ae56e109a8f10eb1b78fa4433ab096c9f1f97657
AeroBytesNL <kelvin@aerobytes.nl>
2025-07-24 16:02:22 +0200

    Added image randomizer

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,15 @@ document.addEventListener("DOMContentLoaded", function () {
         '/assets/img/hero/omp-heli-bg.png',
     ];
 
+    function shuffleArray(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+
+    shuffleArray(images);
+
     images.forEach((src) => {
         const slide = document.createElement("div");
         slide.style.backgroundImage = `url('${src}')`;
