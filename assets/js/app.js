553a14f8161b824eaa3d4a589737670c67044fff
AeroBytesNL <kelvin@aerobytes.nl>
2025-07-24 15:57:39 +0200

    Added slider in hero

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,27 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("footerYear").innerHTML = (new Date()).getFullYear();
+
+    const slider = document.querySelector('.background-slider');
+
+    const images = [
+        '/assets/img/hero/rc-cars-bg.jpg',
+        '/assets/img/hero/rc-plane-bg.jpg',
+        '/assets/img/hero/dji-drone-bg.jpg',
+        '/assets/img/hero/rc-boat-2-bg.png',
+        '/assets/img/hero/omp-heli-bg.png',
+    ];
+
+    images.forEach((src) => {
+        const slide = document.createElement("div");
+        slide.style.backgroundImage = `url('${src}')`;
+        slider.appendChild(slide);
+    });
+
+    let index = 0;
+    function slideNext() {
+        index = (index + 1) % images.length;
+        slider.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(slideNext, 4000);
 })
