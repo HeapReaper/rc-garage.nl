fa842ba155d6a553fb480d07de36f73df74cf5f1
HeapReaper <kelvin@aerobytes.nl>
2025-09-05 16:41:00 +0200

    Made footer use Strapi siteName

--- components/core/footer.tsx
+++ components/core/footer.tsx
@@ -1,7 +1,19 @@
-export default function Footer() {
+async function getData(){
+  const res = await fetch("https://strapi.rc-garage.nl/api/global", {
+    next: {revalidate: 60}
+  });
+
+  const data = await res.json();
+
+  return data.data;
+}
+
+export default async function Footer() {
+  const data = await getData();
+
   return (
     <footer className="text-center py-10 text-gray-500 text-sm">
-      &copy; {new Date().getFullYear()} RC Garage
+      &copy; {new Date().getFullYear()} {data.siteName}
     </footer>
   );
 }
